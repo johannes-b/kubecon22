80acc564b83c4456f152b11206a8820c1513e4d5
Johannes Bräuer <johannes.braeuer@gmx.at>
2022-08-23 12:26:07 +0200

    Update of calculate.js to filter out !200 errors
    Signed-off-by: Johannes Bräuer <johannes.braeuer@gmx.at>

--- fibonacci/k6/calculate.js
+++ fibonacci/k6/calculate.js
@@ -1,8 +1,15 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, fail, sleep } from 'k6';
 
 export default () => {
   const BASE_URL = __ENV.BASE_URL || 'fibo.fibonacci-staging.svc.cluster.local:80';
-  http.get(`http://${BASE_URL}/calculate?num=40`);
+  const res = http.get(`http://${BASE_URL}/calculate?num=40`);
   sleep(1);
+  if (
+    !check(res, {
+      'status code MUST be 200': (res) => res.status == 200,
+    })
+  ) {
+    fail('status code was *not* 200');
+  }
 };
