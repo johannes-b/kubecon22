ad5379fe2db76b49cc5e72094535473ffccba047
Johannes Bräuer <johannes.braeuer@gmx.at>
2022-08-23 20:20:07 +0200

    Add threshold to fail when failure rate >1o percent
    Signed-off-by: Johannes Bräuer <johannes.braeuer@gmx.at>

--- fibonacci/k6/calculate.js
+++ fibonacci/k6/calculate.js
@@ -1,6 +1,14 @@
 import http from 'k6/http';
 import { check, fail, sleep } from 'k6';
 
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.1'], // http errors should be less than 10%
+    // http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
+};
+
 export default () => {
   const BASE_URL = __ENV.BASE_URL || 'fibonacci.fibonacci-staging.svc.cluster.local:80';
   const res = http.get(`http://${BASE_URL}/calculate?num=40`);
